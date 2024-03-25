e44cd202a802f0c593500b474af381879481c1fa
Digvijay Dadaso Patil <digvijaypatil0911@gmail.com>
2024-03-25 16:43:47 +0530

    modify main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
+import 'dart:io';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_demo/notificationservice/local_notification_service.dart';
 import 'package:firebase_demo/view/home.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
@@ -11,12 +11,26 @@ Future<void> backgroundHandler(RemoteMessage message) async {
 }
 
 // ====================================================================
-void main() async {
+
+Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(backgroundHandler);
-  LocalNotificationService.initialize();
-  runApp(MyApp());
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: const FirebaseOptions(
+          apiKey:
+              "AIzaSyBzd6OgdA0TazDw5a10qOqNAb2D9OeIqnw", // google-services.json
+          appId:
+              "1:243115152439:android:cad99fc2e3112f43bb9bb2", // firebase --> project setting --> general
+          messagingSenderId:
+              "243115152439", // firebase --> project setting --> Cloud messaging
+          projectId:
+              "flutter-demo-5d4c2", // firebase --> project setting --> general
+          storageBucket:
+              "flutter-demo-5d4c2.appspot.com", // google-services.json
+        ))
+      : await Firebase.initializeApp();
+
+  runApp(const MyApp());
 }
 
 // ====================================================================
